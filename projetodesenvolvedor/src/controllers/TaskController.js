bdbf7c5bf7368f49a7363aeadc2d503d92288c9d
Mateus Silva Ramos <mateussilva1454@gmail.com>
2025-10-25 11:41:27 -0300

    Implementação do TaskController para coordenar a comunicação entre interface do usuário e os serviços. Funcionalidades implementadas no TaskController: addTask(), deleteTask(), toggleTaskStatus(), getTasksCount(), getTasks(), getAllTasks()

--- projetodesenvolvedor/src/controllers/TaskController.js
+++ projetodesenvolvedor/src/controllers/TaskController.js
@@ -1,1 +1,31 @@
-// modelo mvc
+import { TaskService } from '../services/TaskService.js';
+
+export class TaskController {
+  constructor() {
+    this.taskService = new TaskService();
+  }
+
+  addTask(title, description) {
+    return this.taskService.addTask(title, description);
+  }
+
+  deleteTask(id) {
+    this.taskService.deleteTask(id);
+  }
+
+  toggleTaskStatus(id) {
+    this.taskService.toggleTaskStatus(id);
+  }
+
+  getTasksCount() {
+    return this.taskService.getTasksCount();
+  }
+
+  getTasks(filterType, searchText) {
+    return this.taskService.getTasks(filterType, searchText);
+  }
+
+  getAllTasks() {
+    return this.taskService.getAllTasks();
+  }
+}
