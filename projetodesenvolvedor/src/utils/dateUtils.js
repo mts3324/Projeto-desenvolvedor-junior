03726a48b32d9bc0bf5393fb1bdf4e418234847c
Mateus Silva Ramos <mateussilva1454@gmail.com>
2025-10-25 11:48:06 -0300

    Implementação do dateUtils para padronizar a formatação e o tratamento de dados relacionados a data Funcionalidades implementadas no dateUtils: formatDate(), isToday()

--- projetodesenvolvedor/src/utils/dateUtils.js
+++ projetodesenvolvedor/src/utils/dateUtils.js
@@ -1,1 +1,15 @@
-// modelo mvc   
+export function formatDate(date) {
+  return new Date(date).toLocaleDateString('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+  });
+}
+
+export function isToday(date) {
+  const today = new Date();
+  const compareDate = new Date(date);
+  return today.toDateString() === compareDate.toDateString();
+}
